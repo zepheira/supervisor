ea1cbda28ea4177f6b5b5e99d8f3e98ff5822ba2
Ryan Lee <ryanlee@zepheira.com>
2014-08-18 13:31:16 -0700

    spurious version update to make sure fix to service.rb takes

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@opscode.com"
 license           "Apache 2.0"
 description       "Installs supervisor and provides resources to configure services"
-version           "1.0.0"
+version           "1.0.1"
 
 recipe "z_supervisor", "Installs and configures supervisord"
 
